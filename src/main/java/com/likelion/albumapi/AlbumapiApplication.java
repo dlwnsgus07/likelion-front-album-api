991b31772e337f4dba9dfc304628cc95181735da
JunHyeon <dlwnsgus07@naver.com>
2022-05-21 15:31:32 +0900

    base setting

---
+++ src/main/java/com/likelion/albumapi/AlbumapiApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.albumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumapiApplication.class, args);
+	}
+
+}
