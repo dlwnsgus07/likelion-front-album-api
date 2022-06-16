49106af873366935731fd01e77cb9ace08abab05
JunHyeon <dlwnsgus07@naver.com>
2022-06-17 02:59:00 +0900

    feat : add Api Discription with Swagger

--- src/main/java/com/likelion/albumapi/controller/ArticleController.java
+++ src/main/java/com/likelion/albumapi/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.likelion.albumapi.controller;
 import com.likelion.albumapi.dto.ArticleDto;
 import com.likelion.albumapi.dto.ArticleModifyDto;
 import com.likelion.albumapi.service.ArticleService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.http.HttpStatus;
@@ -22,6 +23,7 @@ public class ArticleController {
     public final ArticleService articleService;
 
     @RequestMapping(value = "articles", method = RequestMethod.GET)
+    @ApiOperation(value = "모든 게시물 조회", notes = "전체 게시물을 조회한다.")
     public ResponseEntity<?> findAll() {
         List<ArticleDto> articleDtoList = new ArrayList<>();
         articleDtoList = articleService.findAll();
@@ -29,32 +31,36 @@ public class ArticleController {
     }
 
     @RequestMapping(value = "create/article", method = RequestMethod.POST)
+    @ApiOperation(value = "게시물 생성", notes = "새로운 게시물을 생성한다")
     public HttpStatus createArticle(MultipartHttpServletRequest mul) throws IOException {
         articleService.createArticle(mul);
         return HttpStatus.OK;
     }
 
     @RequestMapping(value = "{article_id}", method = RequestMethod.GET)
+    @ApiOperation(value = "id에 의한 게시물 조회", notes = "특정 id를 통해 단일 게시물을 찾아온다.")
     public ResponseEntity<?> findArticleById(@PathVariable @Validated Long article_id) {
         ArticleDto articleDto = articleService.findArticleById(article_id);
         return new ResponseEntity<>(articleDto, HttpStatus.OK);
     }
 
     @RequestMapping(value = "delete/article/{article_id}", method = RequestMethod.DELETE)
+    @ApiOperation(value = "게시물 삭제", notes = "특정 id를 통해 게시물을 삭제한다.")
     public HttpStatus deleteArticleById(@PathVariable  @Validated Long article_id) {
         articleService.deleteArticle(article_id);
         return HttpStatus.OK;
     }
 
     @RequestMapping(value = "modify/article/{article_id}", method = RequestMethod.PATCH)
+    @ApiOperation(value = "게시물 수정", notes = "id를 통해 게시물을 수정한다.")
     public HttpStatus modifyArticleById(@PathVariable @Validated Long article_id, @RequestBody ArticleModifyDto articleModifyDto){
-        articleService.updateArticle(article_id, articleModifyDto.getTitle(), articleModifyDto.getContent(), articleModifyDto.getLocalDateTime());
+        articleService.updateArticle(article_id, articleModifyDto.getTitle(), articleModifyDto.getContent());
         return HttpStatus.OK;
     }
     @RequestMapping(value = "update/article/like/{article_id}", method = RequestMethod.PATCH)
+    @ApiOperation(value = "게시물 업데이트", notes = "id를 통해 게시물을 업데이트 한다.")
     public int updateArticleLike(@PathVariable @Validated Long article_id){
         articleService.updateArticleLike(article_id);
-        int countLike = articleService.getArticleLikeCount(article_id);
-        return countLike;
+        return articleService.getArticleLikeCount(article_id);
     }
 }
