c3f5098ef722c34ae6b09c36763161d3db757ff5
JunHyeon <dlwnsgus07@naver.com>
2022-06-20 01:04:19 +0900

    docs: article API 문서 개선

--- src/main/java/com/likelion/albumapi/controller/ArticleController.java
+++ src/main/java/com/likelion/albumapi/controller/ArticleController.java
@@ -4,7 +4,11 @@ import com.likelion.albumapi.dto.*;
 import com.likelion.albumapi.service.ArticleService;
 import com.likelion.albumapi.service.CommentService;
 import com.likelion.albumapi.service.S3Service;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.http.HttpStatus;
@@ -16,6 +20,8 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+@Api(value = "게시물", tags = "게시글 관련 API")
 @CrossOrigin(origins = "http://localhost:8080",allowedHeaders = "*")
 @RestController
 @RequestMapping(value = "/album/")
@@ -25,23 +31,22 @@ public class ArticleController {
     public final S3Service s3Service;
     public final CommentService commentService;
 
+    @Operation(summary = "모든 게시물 조회", description = "데이터 베이스에 존재하는 모든 게시물 정보를 json 형태로 전달 받는다.")
     @RequestMapping(value = "articles", method = RequestMethod.GET)
-    @ApiOperation(value = "모든 게시물 조회", notes = "전체 게시물을 조회한다.")
     public ResponseEntity<?> findAll() {
         List<AllArticleDto> articleDtoList = new ArrayList<>();
         articleDtoList = articleService.findAll();
         return new ResponseEntity<>(articleDtoList, HttpStatus.OK);
     }
-
+    @Operation(summary = "게시물 생성", description = "새로운 게시물을 생성합니다.")
     @RequestMapping(value = "create/article", method = RequestMethod.POST)
-    @ApiOperation(value = "게시물 생성", notes = "새로운 게시물을 생성한다")
     public HttpStatus createArticle(MultipartHttpServletRequest mul) throws IOException {
         articleService.createArticle(mul);
         return HttpStatus.OK;
     }
-
+    @Operation(summary = "id에 의한 게시물 조회", description = "특정 id를 통해 단일 게시물의 내용과 댓글들을 불러온다.")
+    @ApiImplicitParam(name="article_id", value = "정보를 불러올 게시글의 Id 정수로 전달(ex: 1, 2, 5), Router를 통해 전달")
     @RequestMapping(value = "{article_id}", method = RequestMethod.GET)
-    @ApiOperation(value = "id에 의한 게시물 조회", notes = "특정 id를 통해 단일 게시물을 찾아온다.")
     public ResponseEntity<?> findArticleById(@PathVariable @Validated Long article_id) {
         List<CommentSearchDto> commentDtoList = new ArrayList<>();
         commentDtoList = commentService.findAllComment(article_id);
@@ -50,8 +55,9 @@ public class ArticleController {
         return new ResponseEntity<>(articleDto, HttpStatus.OK);
     }
 
+    @Operation(summary = "게시물 삭제", description = "특정 id를 통해 게시물을 삭제한다.")
+    @ApiImplicitParam(name="article_id", value = "특정 게시물의 id를 정수로 전달함 (ex: 1, 2, 5, 10)")
     @RequestMapping(value = "delete/article/{article_id}", method = RequestMethod.DELETE)
-    @ApiOperation(value = "게시물 삭제", notes = "특정 id를 통해 게시물을 삭제한다.")
     public HttpStatus deleteArticleById(@PathVariable  @Validated Long article_id) {
         String fileName = articleService.getFileName(article_id);
         fileName = fileName.replace("https://likelionalbumapi.s3.ap-northeast-2.amazonaws.com/","");
@@ -60,14 +66,19 @@ public class ArticleController {
         return HttpStatus.OK;
     }
 
+    @Operation(summary = "게시물 수정", description = "id를 통해 게시물을 수정한다.")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name="article_id", value = "특정 게시물의 id를 정수로 전달함 (ex: 1, 2, 5, 10)"),
+            @ApiImplicitParam(name = "articleModifyDto", value = "변경할 제목(title), 내용(content)를 Json 형태로 HTTP Body에 싫어 전송")
+    })
     @RequestMapping(value = "modify/article/{article_id}", method = RequestMethod.PATCH)
-    @ApiOperation(value = "게시물 수정", notes = "id를 통해 게시물을 수정한다.")
     public HttpStatus modifyArticleById(@PathVariable @Validated Long article_id, @RequestBody ArticleModifyDto articleModifyDto){
         articleService.updateArticle(article_id, articleModifyDto.getTitle(), articleModifyDto.getContent());
         return HttpStatus.OK;
     }
+    @Operation(summary = "게시물 업데이트", description = "id를 통해 게시물의 좋아요 수를 1개 올려주고.")
+    @ApiImplicitParam(name="article_id", value = "특정 게시물의 id를 정수로 전달함 (ex: 1, 2, 5, 10)")
     @RequestMapping(value = "update/article/like/{article_id}", method = RequestMethod.PATCH)
-    @ApiOperation(value = "게시물 업데이트", notes = "id를 통해 게시물을 업데이트 한다.")
     public int updateArticleLike(@PathVariable @Validated Long article_id){
         articleService.updateArticleLike(article_id);
         return articleService.getArticleLikeCount(article_id);
